9618833e20f428aef1f28a60a9c1a762c8b8c687
Akashsakhare123 <akashsakhare6366@gmail.com>
2023-07-15 19:24:05 +0530

    open video capture

--- code.py
+++ code.py
@@ -3,3 +3,6 @@ import cv2
 
 # Create background subtractor object
 background_subtractor = cv2.createBackgroundSubtractorMOG2()
+
+# Open video capture
+video_capture = cv2.VideoCapture(0)  # You can replace 0 with the video file path
